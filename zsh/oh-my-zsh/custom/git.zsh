78aa2a34660d3a0bd5fd54fbff65a92c1b106d90
gvdp <glenn.vandeputte@partner.alphabet.com>
2021-08-13 16:43:01 +0200

    add function for fuzzy branch finding

--- zsh/oh-my-zsh/custom/git.zsh
+++ zsh/oh-my-zsh/custom/git.zsh
@@ -3,3 +3,8 @@ alias gprune='git branch --merged master | grep -v '"'"'^[ *]*master$'"'"' | xar
 # todo: make this main / master branch aware
 alias gmm="git fetch && git merge origin/master --no-edit && git push"
 alias gcc="gaa && git commit --no-edit && git push"
+
+function gfc() {
+  gf
+  git switch $(git branch -a | fzf | tr -d '[ ]' | sed 's/remotes\/origin\///')
+}
