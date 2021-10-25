623ec14879f665e3f01283140d38cc8c9dbdbf30
gvdp <vdputteglenn@gmail.com>
2021-10-25 22:08:01 +0200

    git prune command

--- zsh/oh-my-zsh/custom/git.zsh
+++ zsh/oh-my-zsh/custom/git.zsh
@@ -1,7 +1,10 @@
-alias gprune='git branch --merged master | grep -v '"'"'^[ *]*master$'"'"' | xargs git branch -d && git remote prune origin'
 
-# todo: make this main / master branch aware
-alias gmm="git fetch && git merge origin/master --no-edit && git push"
+
+function gprune() {
+  echo "pruning with $(git_main_branch)"
+  gbda && git remote prune origin
+}
+alias gmm="git fetch && git merge origin/$(git_main_branch) --no-edit && git push"
 alias gcc="gaa && git commit --no-edit && git push"
 
 function gfc() {
