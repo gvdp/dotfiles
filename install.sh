5e4f879b5ad46b25f983f2ab49c4bfba6c208b1a
gvdp <vdputteglenn@gmail.com>
2021-12-02 11:43:43 +0100

    add install line for vim plug

--- install.sh
+++ install.sh
@@ -120,7 +120,11 @@ setup_homebrew() {
       info "Installing nvm"
       curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
     fi
+
+    # install vim plug
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 }
+
 
 
 function setup_shell() {
