3c96da426e9cd1b25e4e3f1c13fd1d2ad4bc5ef2
gvdp <vdputteglenn@gmail.com>
2023-02-17 22:36:50 +0100

    feat: add prompt to pull dotfile changes§

--- check-status.sh
+++ check-status.sh
@@ -4,8 +4,8 @@
 now=$(date '+%s')
 previous=$([[ -e $DOTFILES/repo-status ]] && head -1 $DOTFILES/repo-status || echo 0)
 elapsed=`expr $now - $previous`
-# interval=10
-interval=3600
+interval=10
+# interval=3600
 
 
 if [[ $elapsed -gt $interval ]] then 
@@ -22,11 +22,16 @@ if [[ $uncommitted -gt 0 ]] then
 		echo "there are uncommitted changes in the dotfiles repo"
 		echo "needs-commit" >> $DOTFILES/repo-status
 else
-		notPulledChanges=$(cd $DOTFILES && git status | grep -c "is behind")
+		notPulledChanges=$(cd $DOTFILES && git status | grep -ce "is behind" -ce "different")
 		
 		if [[ $notPulledChanges -gt 0 ]] then
 				# todo: also prompt for immediately pulling
-				echo "there are unpulled changes in the dotfiles repo"
+				echo "there are unpulled changes in the dotfiles repo"i
+				read "pull?Pull changes?(Y/n)"
+				echo $pull
+				if [[ "$pull" =~ ^[Yy]$ ]] then
+						$(cd $DOTFILES && git pull)
+				fi
 		fi
 fi
 
