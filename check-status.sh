2f8226e91908438399e1ca864498fcc054cdef9c
gvdp <vdputteglenn@gmail.com>
2023-02-17 22:57:28 +0100

    feat: add check for brew dependencies

--- check-status.sh
+++ check-status.sh
@@ -4,8 +4,9 @@
 now=$(date '+%s')
 previous=$([[ -e $DOTFILES/repo-status ]] && head -1 $DOTFILES/repo-status || echo 0)
 elapsed=`expr $now - $previous`
-interval=10
-# interval=3600
+# interval=10
+interval=3600
+brewInterval=86400
 
 
 if [[ $elapsed -gt $interval ]] then 
@@ -36,4 +37,18 @@ else
 fi
 
 
+fi
+
+
+if [[ $elapsed -gt $brewInterval ]] then
+		# todo: also check for inconsistencies with Brewfile
+		outdated=$(brew outdated --json | jq ".formulae | length")
+
+		if [[ $outdated -gt 0 ]] then
+				echo "outdated brew dependencies: $outdated"
+				read "pull?Update versions?(Y/n)"
+				if [[ "$pull" =~ ^[Yy]$ ]] then
+					brew upgrade
+				fi
+		fi
 fi
