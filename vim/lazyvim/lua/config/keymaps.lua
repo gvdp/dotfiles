30d19dfa7d1b3e3b20d11ac2e4539c6edf7a825b
gvdp <vdputteglenn@gmail.com>
2023-08-11 22:31:15 +0200

    feat: initialize lazyvim config

---
+++ vim/lazyvim/lua/config/keymaps.lua
@@ -0,0 +1,25 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+--
+--
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+map("n", "<leader>s", ":update<cr>", { noremap = true })
+map("n", "<leader>q", ":quit<cr>", { noremap = true })
+map("n", "<leader>r", ":source ~/.config/nvim/init.lua<cr>", { noremap = true })
+map("n", "<leader>f", ":! npx prettier --write %<cr>", { noremap = true })
