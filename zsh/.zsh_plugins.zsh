819b9bb4d8420922e4e839c5156d8346c0ae8b2c
gvdp <vdputteglenn@gmail.com>
2023-11-23 21:04:22 +0100

    feat: use antidote to manage zsh plugins

---
+++ zsh/.zsh_plugins.zsh
@@ -0,0 +1,2 @@
+fpath+=( /Users/gvdp/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips )
+source /Users/gvdp/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips/alias-tips.plugin.zsh
