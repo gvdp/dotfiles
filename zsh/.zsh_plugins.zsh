6f82d658647138a1bb671f40b42584e8ed1f98ae
gvdp <vdputteglenn@gmail.com>
2023-11-29 22:31:22 +0100

    fix(zsh): clean up plugin config

--- zsh/.zsh_plugins.zsh
+++ zsh/.zsh_plugins.zsh
@@ -1,2 +1,6 @@
 fpath+=( /Users/gvdp/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips )
 source /Users/gvdp/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips/alias-tips.plugin.zsh
+fpath+=( /Users/gvdp/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /Users/gvdp/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /Users/gvdp/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab )
+source /Users/gvdp/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab/fzf-tab.plugin.zsh
