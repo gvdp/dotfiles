c7e05f9445923e6362a01ae70738342c0c414ca0
gvdp <vdputteglenn@gmail.com>
2024-04-10 22:33:35 +0200

    feat: add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,29 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = "Batman"
+config.color_scheme = "Ocean (dark) (terminal.sexy)"
+
+config.use_fancy_tab_bar = true
+config.window_background_image = wezterm.home_dir .. "/dotfiles/wezterm/backdrops/planet_express.jpg"
+
+config.window_background_image_hsb = {
+	-- Darken the background image by reducing it to 1/3rd
+	brightness = 0.3,
+
+	-- You can adjust the hue by scaling its value.
+	-- a multiplier of 1.0 leaves the value unchanged.
+	hue = 1.0,
+
+	-- You can adjust the saturation also.
+	saturation = 1.0,
+}
+
+-- and finally, return the configuration to wezterm
+return config
