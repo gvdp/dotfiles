edee2ad2683a37b39f10e8dccbbbcb6c113855ef
gvdp <vdputteglenn@gmail.com>
2024-06-11 21:32:22 +0200

    correct antidote load path

--- zsh/.zsh_plugins.zsh
+++
@@ -1,8 +0,0 @@
-fpath+=( /Users/gvdp/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips )
-source /Users/gvdp/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips/alias-tips.plugin.zsh
-fpath+=( /Users/gvdp/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
-source /Users/gvdp/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-fpath+=( /Users/gvdp/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab )
-source /Users/gvdp/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab/fzf-tab.plugin.zsh
-fpath+=( /Users/gvdp/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-jasonmccreary-SLASH-git-trim )
-source /Users/gvdp/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-jasonmccreary-SLASH-git-trim/git-trim.plugin.zsh
